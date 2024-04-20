ee4437ac9558316992c4aa4826cb81ff664472a7
Nicholas Holgate <niceholgate@gmail.com>
2024-04-20 09:48:32 +0800

    Finish QuadTree subdivide method and add boundary/interior iterator methods

--- src/quad_tree.py
+++ src/quad_tree.py
@@ -1,20 +1,20 @@
-from typing import Tuple
+from typing import Tuple, Optional
 import numpy as np
+
 
 class QuadTree:
     x_lims: Tuple[int, int]
     y_lims: Tuple[int, int]
-    nw: 'QuadTree'
-    ne: 'QuadTree'
-    sw: 'QuadTree'
-    se: 'QuadTree'
+    nw: Optional['QuadTree'] = None
+    ne: Optional['QuadTree'] = None
+    sw: Optional['QuadTree'] = None
+    se: Optional['QuadTree'] = None
 
     def __init__(self, x_lims: Tuple[int, int], y_lims: Tuple[int, int]):
         self.x_lims = x_lims
         self.y_lims = y_lims
-        self.boundary_coordinates_generator = self.get_boundary_coordinates_generator()
 
-    def get_boundary_coordinates_generator(self):
+    def boundary_coordinates_generator(self):
         for x in range(self.x_lims[0], self.x_lims[1] + 1):
             yield x, self.y_lims[0]
         for y in range(self.y_lims[0] + 1, self.y_lims[1] + 1):
@@ -24,22 +24,38 @@ class QuadTree:
         for y in reversed(range(self.y_lims[0]+1, self.y_lims[1])):
             yield self.x_lims[0], y
 
-    def iterate_boundary_coordinates(self):
-        return next(self.boundary_coordinates_generator)
+    def interior_coordinates_generator(self):
+        for x in range(self.x_lims[0] + 1, self.x_lims[1]):
+            for y in range(self.y_lims[0] + 1, self.y_lims[1]):
+                yield x, y
 
-    # def subdivide(self):
-    #     x_mid = np.ceil(np.mean(self.x_lims))
-    #     y_mid = np.ceil(np.mean(self.y_lims))
-    #
-    #     # Cannot subdivide a point
-    #     if self.x_lims[0] == self.x_lims[1] and self.y_lims[0] == self.y_lims[1]:
-    #         return
-    #     # For a single column, can only subdivide in y direction
-    #     if self.x_lims[0] == self.x_lims[1]:
-    #         self.nw = QuadTree((x_mid, x_mid), (self.y_lims[0], y_mid))
-    #         self.sw = QuadTree()
-    #     # For a single row, can only subdivide in x direction
-    #
-    #
-    #     self.nw = QuadTree((self.x_lims[0], x_mid), (self.y_lims[0], y_mid))
-    #     self.ne = QuadTree
+    def random_interior_coordinates(self):
+        return np.random.randint(self.x_lims[0] + 1, self.x_lims[1]), np.random.randint(self.y_lims[0] + 1, self.y_lims[1])
+
+    def subdivide(self):
+        x_mid = int(np.floor(np.mean(self.x_lims)))
+        y_mid = int(np.floor(np.mean(self.y_lims)))
+
+        # Cannot subdivide a point
+        if self.x_lims[0] == self.x_lims[1] and self.y_lims[0] == self.y_lims[1]:
+            return
+        # For a single column, can only subdivide in y direction
+        if self.x_lims[0] == self.x_lims[1]:
+            self.nw = QuadTree((x_mid, x_mid), (self.y_lims[0], y_mid))
+            self.sw = QuadTree((x_mid, x_mid), (y_mid+1, self.y_lims[1]))
+            return
+        # For a single row, can only subdivide in y direction
+        if self.y_lims[0] == self.y_lims[1]:
+            self.nw = QuadTree((self.x_lims[0], x_mid), (y_mid, y_mid))
+            self.ne = QuadTree((x_mid+1, self.x_lims[1]), (y_mid, y_mid))
+            return
+        self.nw = QuadTree((self.x_lims[0], x_mid), (self.y_lims[0], y_mid))
+        self.ne = QuadTree((x_mid+1, self.x_lims[1]), (self.y_lims[0], y_mid))
+        self.sw = QuadTree((self.x_lims[0], x_mid), (y_mid+1, self.y_lims[1]))
+        self.se = QuadTree((x_mid+1, self.x_lims[1]), (y_mid+1, self.y_lims[1]))
+
+    def get_children(self):
+        return [child for child in [self.nw, self.ne, self.sw, self.se] if child is not None]
+
+    def __eq__(self, other: 'QuadTree'):
+        return self.x_lims == other.x_lims and self.y_lims == other.y_lims
--- test/quad_tree_test.py
+++ test/quad_tree_test.py
@@ -1,39 +1,99 @@
-import pytest
+# import pytest
 from src.quad_tree import QuadTree
 
 
-def _check_raises(error_type, error_message, func, *args):
-    with pytest.raises(error_type) as excinfo:
-        result = func(*args)
-    assert str(excinfo.value) == error_message
-
-
-def _only_expected_coords_then_stop_iter(sut, expected_boundary_coords):
-    expected_boundary_coords.reverse()
-    while expected_boundary_coords:
-        assert sut.iterate_boundary_coordinates() == expected_boundary_coords.pop()
-    _check_raises(StopIteration, '', sut.iterate_boundary_coordinates)
+# def _check_raises(error_type, error_message, func, *args):
+#     with pytest.raises(error_type) as excinfo:
+#         result = func(*args)
+#     assert str(excinfo.value) == error_message
 
 
 def test_iterate_boundary_coordinates_big():
     sut = QuadTree((0, 2), (0, 3))
     expected_boundary_coords = [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (2, 3), (1, 3), (0, 3), (0, 2), (0, 1)]
-    _only_expected_coords_then_stop_iter(sut, expected_boundary_coords)
+    assert list(sut.boundary_coordinates_generator()) == expected_boundary_coords
 
 
 def test_iterate_boundary_coordinates_2x2():
     sut = QuadTree((0, 1), (0, 1))
     expected_boundary_coords = [(0, 0), (1, 0), (1, 1), (0, 1)]
-    _only_expected_coords_then_stop_iter(sut, expected_boundary_coords)
+    assert list(sut.boundary_coordinates_generator()) == expected_boundary_coords
 
 
 def test_iterate_boundary_coordinates_1x2():
     sut = QuadTree((2, 2), (2, 3))
     expected_boundary_coords = [(2, 2), (2, 3)]
-    _only_expected_coords_then_stop_iter(sut, expected_boundary_coords)
+    assert list(sut.boundary_coordinates_generator()) == expected_boundary_coords
 
 
 def test_iterate_boundary_coordinates_1x1():
     sut = QuadTree((2, 2), (3, 3))
     expected_boundary_coords = [(2, 3)]
-    _only_expected_coords_then_stop_iter(sut, expected_boundary_coords)
+    assert list(sut.boundary_coordinates_generator()) == expected_boundary_coords
+
+
+def test_iterate_interior_coordinates_big():
+    sut = QuadTree((0, 3), (0, 4))
+    expected_interior_coords = [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3)]
+    assert list(sut.interior_coordinates_generator()) == expected_interior_coords
+
+
+def test_iterate_interior_coordinates_2x2():
+    sut = QuadTree((0, 1), (0, 1))
+    expected_interior_coords = []
+    assert list(sut.interior_coordinates_generator()) == expected_interior_coords
+
+
+def test_random_interior_coordinate():
+    sut = QuadTree((0, 3), (0, 4))
+    for _ in range(10):
+        random_coordinates = sut.random_interior_coordinates()
+        assert 0 < random_coordinates[0] < 3
+        assert 0 < random_coordinates[1] < 4
+
+
+def test_subdivide_big_1():
+    sut = QuadTree((0, 2), (0, 3))
+    sut.subdivide()
+    assert sut.get_children() == [QuadTree((0, 1), (0, 1)), QuadTree((2, 2), (0, 1)),
+                                  QuadTree((0, 1), (2, 3)), QuadTree((2, 2), (2, 3))]
+
+
+def test_subdivide_big_2():
+    sut = QuadTree((0, 3), (0, 2))
+    sut.subdivide()
+    assert sut.get_children() == [QuadTree((0, 1), (0, 1)), QuadTree((2, 3), (0, 1)),
+                                  QuadTree((0, 1), (2, 2)), QuadTree((2, 3), (2, 2))]
+
+
+def test_subdivide_2x2():
+    sut = QuadTree((0, 1), (0, 1))
+    sut.subdivide()
+    assert sut.get_children() == [QuadTree((0, 0), (0, 0)), QuadTree((1, 1), (0, 0)),
+                                  QuadTree((0, 0), (1, 1)), QuadTree((1, 1), (1, 1))]
+
+
+def test_subdivide_1x2():
+    sut = QuadTree((0, 0), (0, 1))
+    sut.subdivide()
+    assert sut.ne is None
+    assert sut.se is None
+    assert sut.get_children() == [QuadTree((0, 0), (0, 0)), QuadTree((0, 0), (1, 1))]
+
+
+def test_subdivide_2x1():
+    sut = QuadTree((0, 1), (0, 0))
+    sut.subdivide()
+    assert sut.sw is None
+    assert sut.se is None
+    assert sut.get_children() == [QuadTree((0, 0), (0, 0)), QuadTree((1, 1), (0, 0))]
+
+
+def test_subdivide_1x1():
+    sut = QuadTree((0, 0), (0, 0))
+    sut.subdivide()
+    assert sut.nw is None
+    assert sut.ne is None
+    assert sut.sw is None
+    assert sut.se is None
+    assert sut.get_children() == []
