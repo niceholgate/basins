ac5e10a32dc00bf3b5c72f904a00f29a7f56b505
Nicholas Holgate <niceholgate@gmail.com>
2024-10-23 00:00:53 +0800

    Make the initial solution search limits a request parameter

---
+++ test/utils_test.py
@@ -0,0 +1,11 @@
+import src.utils as sut
+import numpy as np
+
+
+def test_manhattan_distance():
+    assert sut.manhattan_distance(np.array([-4, 1]), np.array([6, 3])) == 10 + 2
+
+
+def test_mean_manhattan_distance_between_group_of_points():
+    points = [np.array([0, 0]), np.array([2, 3]), np.array([-1, -1]), np.array([1, 1])]
+    assert sut.mean_manhattan_distance_between_group_of_points(points) == (5 + 2 + 7 + 2 + 3 + 4)/6
